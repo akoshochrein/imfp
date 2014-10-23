7b0078d7ab97328fdec04dc3b8784109e9608a4f
akoskaaa <akos.hochrein@prezi.com>
2014-10-23 21:20:24 +0200

    removed some fields I will not use yet

--- imfp/events/models.py
+++ imfp/events/models.py
@@ -1,18 +1,16 @@
 from django.contrib.auth.models import User
 from django.db import models
-from django_google_maps import fields as map_fields
 from imfp.events.constants import EVENT_TYPES, EVENT_ZONES
 
 
 class EventManager(models.Manager):
 
-    def create_event(self, creator_user_id, description, creation_time, time, address, location, zone, type, seats):
+    def create_event(self, creator_user_id, description, creation_time, time, location, zone, type, seats):
         event = self.create(
             creator=User.objects.get(creator_user_id),
             description=description,
             creation_time=creation_time,
             time=time,
-            address=address,
             location=location,
             zone=zone,
             type=type,
@@ -36,9 +34,8 @@ class Event(models.Model):
     description = models.CharField(max_length=255)
     creation_time = models.DateTimeField()
     time = models.DateTimeField()
-    # todo figure out base fields here
-    address = map_fields.AddressField(max_length=200)
-    location = map_fields.GeoLocationField(max_length=100)
+    # todo figure out geodata with django_google_maps fields here
+    address = models.TextField(max_length=200)
     type = models.CharField(max_length=32, choices=EVENT_TYPES)
     zone = models.CharField(max_length=32, choices=EVENT_ZONES)
     seats = models.IntegerField()

1164b3e1b600729e574a557b6c2d287a294f505d
akoskaaa <akos.hochrein@prezi.com>
2014-11-09 16:16:51 +0100

    make tests happy

--- imfp/events/forms.py
+++ imfp/events/forms.py
@@ -3,10 +3,11 @@ from imfp.events.constants import EVENT_TYPES, EVENT_ZONES
 
 
 class CreateEventForm(forms.Form):
+    user_id = forms.IntegerField()
     name = forms.CharField(max_length=255)
     description = forms.CharField(max_length=255)
     seats = forms.IntegerField()
-    time = forms.DateTimeField()
+    creation_time = forms.DateTimeField()
     type = forms.ChoiceField(choices=EVENT_TYPES)
     zone = forms.ChoiceField(choices=EVENT_ZONES)
 

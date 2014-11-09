0c0d7c10752e74409d87a3c25f9633047a5c6668
akoskaaa <akos.hochrein@prezi.com>
2014-11-09 21:15:42 +0100

    add urls for events

--- imfp/events/urls.py
+++ imfp/events/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns
 import api
 import views
 
-urlpatterns = patterns('event',
+urlpatterns = patterns('events',
                        (r'^create/$', api.create_event,),
                        (r'^subscribe/(?P<event_id>\d+)$', api.subscribe_to_event,),
                        (r'^unsubscribe/(?P<event_id>\d+)$', api.unsubscribe_from_event,),
--- imfp/imfp/urls.py
+++ imfp/imfp/urls.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.conf.urls import patterns, include
 from django.contrib import admin
 
@@ -6,3 +7,8 @@ admin.autodiscover()
 urlpatterns = patterns('',
                        (r'^admin/', include(admin.site.urls)),
                        )
+
+if 'events' in settings.INSTALLED_APPS:
+    urlpatterns += patterns('',
+                            (r'^events/', 'events.urls')
+                            )
